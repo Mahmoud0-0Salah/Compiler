0300d801e96eeeb8abd5af38f2da65d82e8a7d43
Mahmoud0-0Salah <mahmoudsalah2339@gmail.com>
2024-11-03 13:01:41 +0200

    token extractor

---
+++ Compiler/WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            txt1 = new TextBox();
+            txt2 = new TextBox();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(185, 229, 232);
+            button1.Font = new Font("Segoe UI", 19.8000011F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button1.ForeColor = Color.Black;
+            button1.Location = new Point(1272, 130);
+            button1.Name = "button1";
+            button1.Size = new Size(251, 94);
+            button1.TabIndex = 0;
+            button1.Text = "Tokens";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.FromArgb(185, 229, 232);
+            button2.Font = new Font("Segoe UI", 19.8000011F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button2.ForeColor = Color.Black;
+            button2.Location = new Point(1272, 230);
+            button2.Name = "button2";
+            button2.Size = new Size(251, 94);
+            button2.TabIndex = 1;
+            button2.Text = "Grammar";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // txt1
+            // 
+            txt1.BackColor = Color.FromArgb(223, 242, 235);
+            txt1.Font = new Font("Segoe UI", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txt1.Location = new Point(1, 130);
+            txt1.Multiline = true;
+            txt1.Name = "txt1";
+            txt1.ScrollBars = ScrollBars.Both;
+            txt1.Size = new Size(1265, 391);
+            txt1.TabIndex = 2;
+            txt1.TextChanged += textBox1_TextChanged;
+            // 
+            // txt2
+            // 
+            txt2.BackColor = Color.FromArgb(223, 242, 235);
+            txt2.Font = new Font("Segoe UI", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txt2.Location = new Point(1, 527);
+            txt2.Multiline = true;
+            txt2.Name = "txt2";
+            txt2.ScrollBars = ScrollBars.Both;
+            txt2.Size = new Size(1265, 250);
+            txt2.TabIndex = 3;
+            txt2.TextChanged += textBox2_TextChanged;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(122, 178, 211);
+            ClientSize = new Size(1525, 789);
+            Controls.Add(txt2);
+            Controls.Add(txt1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Compiler";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private TextBox txt1;
+        private TextBox txt2;
+    }
+}
---
+++ Compiler/WinFormsApp1/Form1.cs
@@ -0,0 +1,68 @@
+using System.Text.RegularExpressions;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            Label titleLabel = new Label();
+            titleLabel.Text = "Compiler";
+            titleLabel.Font = new Font("Arial", 24, FontStyle.Bold);
+            titleLabel.TextAlign = ContentAlignment.MiddleCenter;
+            titleLabel.Dock = DockStyle.Top;
+            titleLabel.Height = 50;
+
+            this.Controls.Add(titleLabel);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var Tokens = new[] {
+                ("KEYWORD", @"^(if|else|for|int|float|double|while|do|return)"),
+                ("ID", @"^[A-Za-z_](\w)*$"),
+                ("NUM", @"^(-|\+)?(\d+)(\.(\d+))?([eE][-\+]?\d+)?$"),
+                ("DELIMITER", @"[();{}\[\]]"),
+                ("OP", @"(\+|/|-|\*|=|<|>|<=|>=|\||&|\^)")
+            };
+            var input = txt1.Text;
+            input = Regex.Replace(input, $@"{Tokens[3].Item2}", match => $" {match.Value} ");
+            string[] result = Regex.Split(input, @"\s+");
+
+            txt2.Clear();  
+            foreach (string s in result)
+            {
+                foreach (var token in Tokens)
+                {
+                    if (Regex.IsMatch(s, token.Item2))
+                    {
+                        txt2.AppendText($"{s} => {token.Item1}{Environment.NewLine}");
+                        break;
+                    }
+                }
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
