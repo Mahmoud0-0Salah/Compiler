c397a9420985b53c532ce47ac9e7f8c629bb3b85
Mahmoud0-0Salah <mahmoudsalah2339@gmail.com>
2024-11-12 15:26:27 +0200

    Edit in tests

--- Compiler/ParserTest/UnitTest1.cs
+++ Compiler/ParserTest/UnitTest1.cs
@@ -12,8 +12,9 @@ namespace ParserTest
         [InlineData("من (س الي ص  + ض ) \r\n{\r\n\r\n}", "Parser Output:\r\nParsing successful! Rules called:\r\nLoop\r\n")]
         [InlineData("اذا (س <= ص  + ض) \r\n{\r\n\r\n}", "Parser Output:\r\nParsing successful! Rules called:\r\nConditionStmt\r\n")]
         [InlineData("س = ص  (  ض ) ;\r\n\r\n س = ص ;", "Parser Output:\r\nParsing successful! Rules called:\r\nAssign\r\nAssign\r\n")]
-        [InlineData("صحيح دالة (صحيح س) \r\n{\r\n  صحيح ص = 2 ;\r\n  من (ص الي س / 2 ) \r\n  {\r\n      اذا ( س % ص == 0 ) \r\n      {\r\n          ارجع 0 ;\r\n      }\r\n      ارجع 1 ;\r\n  }\r\n}  \r\n\r\nصحيح س = 13 ;\r\n\r\nصحيح اولي  = دالة ( س ) ;\r\n\r\nصحيح اولي  = دالة ( ١٢  ) ;\r\n\r\nاطبع (  زوجي) ;\r\n"
-            , "Parser Output:\r\nParsing successful! Rules called:\r\nDeclaration\r\nDeclaration\r\nLoop\r\nConditionStmt\r\nFunc_Return\r\nFunc_Return\r\nDeclaration\r\nDeclaration\r\nDeclaration\r\nPrint\r\n")]
+        [InlineData("اطبع (س) ;\r\n\r\nاطبع (١٢) ;", "Parser Output:\r\nParsing successful! Rules called:\r\nPrint\r\nPrint\r\n")]
+        [InlineData("صحيح دالة (صحيح س) \r\n{\r\n  صحيح ص = 2 ;\r\n  من (ص الي س / 2 ) \r\n  {\r\n      اذا ( س % ص == 0 ) \r\n      {\r\n          ارجع 0 ;\r\n      }\r\n      ارجع 1 ;\r\n  }\r\n}  \r\n\r\nصحيح س = 13 ;\r\n\r\nصحيح اولي  = دالة ( س ) ;\r\n\r\n اولي  = دالة ( ١٢  ) ;\r\n\r\nاطبع (  اولي) ;"
+            , "Parser Output:\r\nParsing successful! Rules called:\r\nDeclaration\r\nDeclaration\r\nLoop\r\nConditionStmt\r\nFunc_Return\r\nFunc_Return\r\nDeclaration\r\nDeclaration\r\nAssign\r\nPrint\r\n")]
         public void Test1(string input,string exp)
         {
             var form = new Form1();
