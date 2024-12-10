6f2008b527593b9c6c24267f3b309273727032a1
Mahmoud0-0Salah <mahmoudsalah2339@gmail.com>
2024-12-10 21:46:38 +0200

    Edit in rules

--- Compiler/ParserTest/UnitTest1.cs
+++ Compiler/ParserTest/UnitTest1.cs
@@ -7,8 +7,8 @@ namespace ParserTest
     public class UnitTest1
     {
         [Theory]
-        [InlineData("صحيح  س = 2 ;\r\n\r\nكلمة س [ 33 ] ;","Parser Output:\r\nParsing successful! Rules called:\r\nDeclaration\r\nDeclaration\r\n")]
-        [InlineData("صحيح دالة (صحيح س , عائم ص) \r\n{\r\n\r\n}", "Parser Output:\r\nParsing successful! Rules called:\r\nDeclaration\r\n")]
+        [InlineData("صحيح  س = 2 ;\r\n\r\nمتغير س [ 33 ] ;", "Parser Output:\r\nParsing successful! Rules called:\r\nDeclaration\r\nDeclaration\r\n")]
+        [InlineData("صحيح دالة (صحيح س , صحيح ص) \r\n{\r\n\r\n}", "Parser Output:\r\nParsing successful! Rules called:\r\nDeclaration\r\n")]
         [InlineData("من ( صحيح ش في   ( س الي ص  + ض  )  ) \r\n{\r\n\r\n}", "Parser Output:\r\nParsing successful! Rules called:\r\nLoop\r\n")]
         [InlineData("اذا (س <= ص  + ض) \r\n{\r\n\r\n}\r\n\r\nاخر\r\n{\r\n\r\n\r\n}", "Parser Output:\r\nParsing successful! Rules called:\r\nConditionStmt\r\n")]
         [InlineData("س = ص  (  ض ) ;\r\n\r\n س = ص ;", "Parser Output:\r\nParsing successful! Rules called:\r\nAssign\r\nAssign\r\n")]
